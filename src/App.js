1fb561d5609d882bce1f2202e3c37fabc43ceab8
Anthonny Tineo Ruiz <anthonnytineo@MacBook-Pro-de-Anthonny.local>
2021-06-06 19:53:54 -0400

    armando la estructura

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [selectColor, setselectcolor] = useState ("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="home">
+      <div onClick={() => setselectcolor ("red")} 
+        className={"light red" + (selectColor === "red" ? " glow" : "")}></div>
+      <div onClick={() => setselectcolor ("yellow")}
+        className={"light yellow" + (selectColor === "yellow" ? " glow" : "")}></div>
+      <div onClick={() => setselectcolor ("green")}
+        className={"light green" + (selectColor === "green" ? " glow" : "")}></div>
     </div>
   );
 }
