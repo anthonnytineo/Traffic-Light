468b637aaf9b9be4aee164ce63aaeb29292a4f7d
Anthonny Tineo Ruiz <anthonnytineo@MacBook-Pro-de-Anthonny.local>
2021-06-06 22:57:29 -0400

    clases de css

--- src/App.js
+++ src/App.js
@@ -6,13 +6,20 @@ function App() {
   const [selectColor, setselectcolor] = useState ("");
 
   return (
-    <div className="home">
-      <div onClick={() => setselectcolor ("red")} 
-        className={"light red" + (selectColor === "red" ? " glow" : "")}></div>
-      <div onClick={() => setselectcolor ("yellow")}
-        className={"light yellow" + (selectColor === "yellow" ? " glow" : "")}></div>
-      <div onClick={() => setselectcolor ("green")}
-        className={"light green" + (selectColor === "green" ? " glow" : "")}></div>
+    <div className="container">
+        <div className="row">
+          <div className="col-3">""""""""""</div>
+          <div className="intubo col-4">.<br/><br/><br/></div>
+          <div className="col-3">""</div>
+        </div>
+      <div className="home">
+        <div onClick={() => setselectcolor ("red")} 
+          className={"light red" + (selectColor === "red" ? " glow" : "")}></div>
+        <div onClick={() => setselectcolor ("yellow")}
+          className={"light yellow" + (selectColor === "yellow" ? " glow" : "")}></div>
+        <div onClick={() => setselectcolor ("green")}
+          className={"light green" + (selectColor === "green" ? " glow" : "")}></div>
+      </div>
     </div>
   );
 }
